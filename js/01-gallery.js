7a07bd0f1b05fa357753d96a19e6061bee2d248c
AlexSBolotov <122620169+AlexSBolotov@users.noreply.github.com>
2023-03-21 10:24:59 +0200

    before_mentor's_check

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// galleryItems.forEach((el) => console.log(el.description));
+
+const galleryContainer = document.querySelector(".gallery");
+
+const createGalleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+        <a class="gallery__link" href='${original}'>
+            <img
+            class="gallery__image"
+            src='${preview}'
+            data-source='${original}'
+            alt='${description}'
+            />
+        </a>
+    </li>`
+  )
+  .join("");
+
+galleryContainer.insertAdjacentHTML("afterbegin", createGalleryMarkup);
+
+galleryContainer.addEventListener("click", openLargeImage);
+
+function openLargeImage(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+
+  window.addEventListener("keydown", closeLargeImageByEscape);
+  function closeLargeImageByEscape(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+const makeGalleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`
+  )
+  .join("");
+
+galleryContainer.insertAdjacentHTML("afterbegin", makeGalleryMarkup);
+
+let gallery = new SimpleLightbox(".gallery a");
+// console.log(gallery);
+gallery.defaultOptions.captionsData = "alt";
+gallery.defaultOptions.captionDelay = "250";
